a8bc1d2ef03704c4d515be6815744349b72e37ce
Ben Harrington <ben.harrington@manheim.com>
2014-07-23 06:48:00 -0400

    Palindrome Java

--- palindrome/Palindrome.java
+++ palindrome/Palindrome.java
@@ -1,13 +1,14 @@
 public class Palindrome {
 
-  public static boolean palindrome(String str) {
-    return str.equals(new StringBuilder(str).reverse().toString());
+  public static boolean palindrome(String word) {
+    String reverse = new StringBuffer(word).reverse().toString();
+    return word.equals(reverse);
   }
 
   public static void main(String[] args) {
-    //Palindrome
+    // Palindrome
     System.out.println(palindrome("bob"));
-    //Not a Palindrome
+    // Not a Palindrome
     System.out.println(palindrome("qwerty"));
   }
 
