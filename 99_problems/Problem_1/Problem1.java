5ca1d424443228bab8f645a93b9f5d92626ddf52
Ben Harrington <ben.harrington@manheim.com>
2014-07-24 12:19:14 -0400

    Adding java and python Problem1

---
+++ 99_problems/Problem_1/Problem1.java
@@ -0,0 +1,10 @@
+public class Problem1 {
+
+  public static void main(String[] args) {
+    int[] arr = {1,2,3,4};
+    System.out.println(arr[arr.length-1]);
+    char[] carr = {'a', 'b', 'c', 'd'};
+    System.out.println(carr[carr.length-1]);
+  }
+
+}
---
+++ 99_problems/Problem_1/problem1.py
@@ -0,0 +1,4 @@
+arr = [1,2,3,4]
+carr = ["a", "b", "c", "d"]
+print arr[-1]
+print carr[-1]
