54f587f085eced5a37a286e8995cadc96bcf95e8
Ben Harrington <ben.harrington@manheim.com>
2014-07-24 13:27:59 -0400

    adding problem 1 c# and perl

---
+++ 99_problems/Problem_1/problem1.cs
@@ -0,0 +1,14 @@
+using System;
+namespace Problem1
+{
+  class Problem1 
+  {
+    static void Main() 
+    {
+      int[] arr = {1, 2, 3, 4};
+      Console.WriteLine(arr[arr.Length-1]);
+      char[] car = {'a', 'b', 'c', 'd'};
+      Console.WriteLine(car[car.Length-1]);
+     }
+  }
+}
---
+++ 99_problems/Problem_1/problem1.pl
@@ -0,0 +1,7 @@
+#!/usr/bin/perl
+
+@arr = (1, 2, 3, 4);
+print "$arr[-1]\n";
+
+@car = ("a", "b", "c", "d");
+print "$car[-1]\n";
